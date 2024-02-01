94df93bd62e1db4e7b9e704b71ce8194328352b6
Kevin Suñer <kevin.suner@proton.me>
2024-02-01 17:03:11 +0100

    turning off echoing and disabling raw mode at exit

--- src/piccolo.zig
+++ src/piccolo.zig
@@ -1,14 +1,42 @@
 const std = @import("std");
 const debug = std.debug;
 const fs = std.fs;
+const os = std.os;
+
+const Terminal = struct {
+	tty: fs.File,
+	og_termios: os.termios,
+};
+
+fn disableRawMode(self: *Terminal) void {
+	_ = os.tcsetattr(self.tty.handle, .FLUSH, self.og_termios) catch |err| {
+		debug.print("{}\n", .{err});
+	};
+}
+
+fn enableRawMode(self: *Terminal) !void {
+	self.og_termios = try os.tcgetattr(self.tty.handle);
+
+	var raw = self.og_termios;
+	raw.lflag &= ~@as(
+		os.linux.tcflag_t,
+		os.linux.ECHO,
+	);
+
+	try os.tcsetattr(self.tty.handle, .FLUSH, raw);
+}
 
 pub fn main() !void {
 	var tty = try fs.cwd().openFile("/dev/tty", .{ .mode = .read_write });
 	defer tty.close();
 
+	var term = Terminal{ .tty = tty, .og_termios = undefined };
+	try enableRawMode(&term);
+	defer disableRawMode(&term);
+
 	while (true) {
 		var buffer: [1]u8 = undefined;
-		_ = try tty.read(&buffer);
+		_ = try term.tty.read(&buffer);
 		if (buffer[0] == 'q') {
 			return;
 		} else {
