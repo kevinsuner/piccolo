de00fb89223f0e392e3bb2bf46f6f13d83b80196
Kevin Suñer <kevin.suner@proton.me>
2024-02-01 15:58:51 +0100

    reading tty to get user input

--- src/piccolo.zig
+++ src/piccolo.zig
@@ -1,27 +1,18 @@
 const std = @import("std");
+const debug = std.debug;
+const fs = std.fs;
 
 pub fn main() !void {
-	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-	const allocator = gpa.allocator();
-	defer _ = gpa.deinit();
-
-	const in = std.io.getStdIn();
-	var buf = std.io.bufferedReader(in.reader());
-	var r = buf.reader();
-
-	var input = std.ArrayList(u8).init(allocator);
-	defer input.deinit();
+	var tty = try fs.cwd().openFile("/dev/tty", .{ .mode = .read_write });
+	defer tty.close();
 
 	while (true) {
-		const b = r.readByte() catch |err| switch (err) {
-			error.EndOfStream => break,
-			else => return err, 
-		};
-
-		if (b == 'q') {
-			break;
+		var buffer: [1]u8 = undefined;
+		_ = try tty.read(&buffer);
+		if (buffer[0] == 'q') {
+			return;
+		} else {
+			debug.print("input: {} {s}\r\n", .{ buffer[0], buffer });
 		}
 	}
-	
-	return;
 }
